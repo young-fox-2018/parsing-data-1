e2ebe62ddc61ba12c927a6b6729a44680440dd27
Kevin Wijaya <Kevin@Macbooknyaa.localdomain>
2018-11-05 15:15:16 +0700

    refactoring

--- parser.js
+++ parser.js
@@ -18,42 +18,43 @@ class Person {
 class PersonParser {
 
   constructor(file) {
-    this._file = file.split("\n");
-    this._file2 = this.commaSeparating();
+    this._file = file;
     this._people = this.generatePeople();
   }
 
-  commaSeparating(){
-    let array = []
-    for(let i = 0 ; i < this._file.length; i++){
-      array.push(this._file[i].split(","))
-    }
-
-    return array;
-  }
 
 
   get people() {
     return this._people
   }
 
-  addPerson(id, first_name, last_name, email, phone, created_at) {
-    let result = new Person(id, first_name, last_name, email, phone, created_at);
-
-    this._people.push(result)
-
-    this.save()
-
+  get file(){
+    return this._file
   }
 
+  addPerson(newPerson) {
+    this._people.push(newPerson)
+
+    this.save()
+    
+  }
+  
   generatePeople() {
-    let result = []
-    for(let i = 1 ; i < this._file2.length; i++){
-      let j = new Person(this._file2[i][0],this._file2[i][1],this._file2[i][2],this._file2[i][3],this._file2[i][4],this._file2[i][5])
-      result.push(j)
+    let data = fs.readFileSync(`./${this._file}`, 'utf8').trim().split("\n")
+    
+    let array = []
+    for(let i = 0 ; i < data.length; i++){
+      array.push(data[i].split(","))
     }
 
+    
 
+
+    let result = []
+    for(let i = 1 ; i < array.length; i++){
+      let j = new Person(array[i][0],array[i][1],array[i][2],array[i][3],array[i][4],array[i][5])
+      result.push(j)
+    }
     return result
 
   }
@@ -62,9 +63,9 @@ class PersonParser {
     let result = "";
 
     result += "id,first_name,last_name,email,phone,created_at \n"
-    for(let i = 0 ; i < this.people.length; i++){
+    for(let i = 0 ; i < this._people.length; i++){
       let cont = "";
-      cont += `${this.people[i]["id"]},${this.people[i]["first_name"]},${this.people[i]["last_name"]},${this.people[i]["email"]},${this.people[i]["phone"]},${this.people[i]["created_at"]} \n`
+      cont += `${this._people[i]["id"]},${this._people[i]["first_name"]},${this._people[i]["last_name"]},${this._people[i]["email"]},${this._people[i]["phone"]},${this._people[i]["created_at"]} \n`
       result += cont;
     }
     // console.log(result)
@@ -74,12 +75,10 @@ class PersonParser {
 
 }
 
-let peoplecsv = fs.readFileSync('./people.csv', 'utf8').trim();
-let parser = new PersonParser(peoplecsv);
+let parser = new PersonParser('people.csv');
 
-parser.commaSeparating()
-
-parser.addPerson(201, "Kevin", "Wijaya", "kevintanuhardi@gmail.com", "", new Date())
+parser.addPerson(new Person (201, "Kevin", "Wijaya", "kevintanuhardi@gmail.com", "", new Date()))
+// console.log(parser.people)
 
 
-// console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`)
+console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`)
