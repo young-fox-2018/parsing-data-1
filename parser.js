d8a90176dee30a6689d39a9e2c35ab9630050cab
Desy Armariena <desy.armariena@gmail.com>
2018-11-05 15:17:59 +0700

    Parsing data into csv

--- parser.js
+++ parser.js
@@ -1,25 +1,23 @@
 "use strict"
 
-class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+const Person = require('./Person.js');
+const PersonParser = require('./PersonParser.js');
+
+const parser = new PersonParser('people.csv')
+
+const obj = {
+  id: 201,
+  firstName: 'Desy',
+  lastName: 'Rachmawati',
+  email: 'desy.armariena@gmail.com',
+  phone: '0853',
+  created: '2015-03-25T12:00:00Z'
 }
 
-class PersonParser {
-
-  constructor(file) {
-    this._file = file
-    this._people = null
-  }
-
-  get people() {
-    return this._people
-  }
-
-  addPerson() {}
-
-}
-
-let parser = new PersonParser('people.csv')
-
+let person1 = new Person(obj);
+console.log(`List people:`, parser.people);
 console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`)
+parser.addPerson(person1);
+
+console.log(`New list people:`, parser.people);
+parser.save();
