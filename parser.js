503229916f2f03ebb385d0367dd149a1fd42a9c7
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-11-05 21:21:49 +0700

    Parsing Data-Done

--- parser.js
+++ parser.js
@@ -1,25 +1,52 @@
 "use strict"
+const Person = require("./person")
+var fs = require('fs')
 
-class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
-}
 
 class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = this.convert()
   }
 
   get people() {
     return this._people
   }
 
-  addPerson() {}
 
+  addPerson(param) {
+    this._people.push(param)
+  }
+  writeFile(file, data) {
+    fs.writeFileSync(file, data)
+  }
+
+  save() {
+    var result = ""
+    let key = Object.keys(this._people[0]).join(",")
+    result = key + "\n"
+    for (let i = 0; i < this._people.length; i++) {
+      let values = Object.values(this._people[i]);
+      let join = values.join(',');
+      result += join + "\n"
+    }
+    this.writeFile("people.csv", result)
+  }
+
+  convert() {
+    let peopleData = fs.readFileSync(this._file, "utf8")
+    let data = peopleData.split("\n")
+    let convertResult = []
+    for (let i = 1; i < data.length; i++) {
+      let dataSplit = data[i].split(",")
+      let convertData = new Person(dataSplit[0], dataSplit[1], dataSplit[2], dataSplit[3], dataSplit[4], dataSplit[5])
+      convertResult.push(convertData)
+    }
+    return convertResult
+  }
 }
-
 let parser = new PersonParser('people.csv')
+parser.addPerson(new Person("201", "Lamda", "Rollingston", "bandit@quam.com", "1 - 633 - 389 - 7173", new Date().toISOString()))
+parser.save()
 
-console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`)
---
+++ person.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(id, first_name, last_name, email, phone, created_at) {
+        this.id = id
+        this.first_name = first_name
+        this.last_name = last_name
+        this.email = email
+        this.phone = phone
+        this.created_at = created_at
+    }
+
+}
+
+module.exports = Person
